c8067793ba6db3ae6f3a1e6ab37af787f82aeb90
Eirik Akerhaugen <eirik_akerhaugen@hotmail.com>
2026-08-31 23:29:24 +0200

    Added sending udp packets from one machine to another every 2ms of audio captured

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::Result, thread, time::Duration};
+use std::{env, io::{Result, stdin}, net::{IpAddr, SocketAddr, UdpSocket}, thread, time::Duration};
 use cpal::{StreamConfig, traits::{DeviceTrait, HostTrait, StreamTrait}};
 use ringbuf::{HeapRb, traits::*};
 
@@ -9,46 +9,33 @@ use ringbuf::{HeapRb, traits::*};
 // }
 
 
+fn get_local_ip() -> Result<(IpAddr)> {
 
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
+    socket.connect("8.8.8.8:80")?;
+    
+    let local_addr = socket.local_addr()?;
+    Ok(local_addr.ip())
+}
 
-fn main() -> Result<()> {
+fn print_help() {
+    println!("#----------------------------------------------#");
+    println!("Please provide if you are a client or server");
+    println!("#----------------------------------------------#");
+}
+
+fn run_server() -> Result<()> {
+    // Handle audio output
     let host: cpal::Host = cpal::default_host();
-    let input_device: cpal::Device = host.default_input_device().expect("No Input device available");
-
-    let input_supported_config = input_device.default_input_config().expect("Error while querying input configs");
-    let input_config: StreamConfig = input_supported_config.into();
-
-    // Make an array that is 4 sec long of 48000hz audio and split it so that one can push and one cat pop
-    let rb = HeapRb::<i16>::new(48000 * 8);
-    let (mut producer, mut consumer) = rb.split();
-
-    println!("You can now speak to yourself for 10 sec");
-    let input_stream = input_device.build_input_stream(
-        input_config, 
-        move |data: &[i16], _: &cpal::InputCallbackInfo| {
-            // Read stream input audio
-            for &value in data {
-                producer.try_push(value).unwrap();
-            }
-
-        }, 
-        move|err|{
-            // React to errors
-            eprintln!("Input stream error: {}", err);
-        }, 
-        None 
-        // Timeout for stream initialization: None = wait indefinitively. Some(Duration) = time to wait for the backend
-    ).expect("Failed to unwrap the input stream");
-
-    input_stream.play().expect("Failed to start recording input audio");
-
-        
     let output_device: cpal::Device = host.default_output_device().expect("No output device available");
     let output_supported_config = output_device.default_output_config().expect("Error while querying output configs");
     let output_config: StreamConfig = output_supported_config.into();
 
-
     let output_channels = output_config.channels as usize;
+
+    // Make ring_buffer that can hold 1 sec of 48kHz with 2 channels
+    let ring_buffer = HeapRb::<i16>::new(48000 * 2);
+    let (mut producer, mut consumer) = ring_buffer.split();
 
     let output_stream = output_device.build_output_stream(
         output_config, 
@@ -75,8 +62,141 @@ fn main() -> Result<()> {
 
     output_stream.play().expect("Failed to playback the audio");
 
-    thread::sleep(Duration::from_secs(10));
-    drop(output_stream);
+    let local_ip = match get_local_ip() {
+    Ok(ip) => Some(ip),
+    Err(err) => {
+        eprintln!("Error getting local IP: {:?}", err);
+        None
+    }
+    }.expect("Could not get local ip address");
+
+    println!("Your local ip is {}",local_ip);
+
+
+    let addr: &str = "0.0.0.0:9999";
+    let socket = UdpSocket::bind(addr)?;
+    println!("This machine has started listening for UDP connections on: {}", addr);
+    socket.set_nonblocking(true)?;
+
+    let mut udp_producer = producer;
+    thread::spawn(move || {
+        let mut buf = [0u8; 1960];
+
+        loop {
+            match socket.recv_from(&mut buf) {
+                Ok((len, src)) => {
+                    let samples_count = len / 2;
+
+                    for index in 0..samples_count {
+                        let b0 = buf[index * 2];
+                        let b1 = buf[index * 2 + 1];
+                        let sample = i16::from_le_bytes([b0, b1]);
+
+                        let _ = udp_producer.try_push(sample);
+
+                        println!("Received '{}' bytes from'{}'", len, src);
+                    }
+                }
+                Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                    thread::sleep(Duration::from_millis(5));
+                }
+                Err(e) => {
+                    eprintln!("Error with receiving udp packet: {}", e);
+                }
+            }
+        }
+    });
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let mut job: String =  String::new();
+
+    if args.len() == 1 {
+        job = args[1].to_lowercase();
+    } else {
+        print_help();
+    }
+
+    if job == "-h" || job == "help" {
+        print_help();
+    } else if job == "server" {
+        if let Err(e) = run_server() { eprintln!("Server error: {}", e); }
+    }
+
+    let local_ip = match get_local_ip() {
+        Ok(ip) => Some(ip),
+        Err(err) => {
+            eprintln!("Error getting local IP: {:?}", err);
+            None
+        }
+    }.expect("Could not get local ip address");
+    
+    println!("Your local ip is {}",local_ip);
+
+
+    // Handle audio input
+    let host: cpal::Host = cpal::default_host();
+    let input_device: cpal::Device = host.default_input_device().expect("No Input device available");
+
+    let input_supported_config = input_device.default_input_config().expect("Error while querying input configs");
+    let input_config: StreamConfig = input_supported_config.into();
+
+    
+
+    const BYTES_PER_SAMPLE: usize = 2;
+    const TARGET_BYTES: usize = 1960;
+    const TARGET_SAMPLES: usize = TARGET_BYTES / BYTES_PER_SAMPLE;
+
+
+    let input_stream = input_device.build_input_stream(
+        input_config,
+        {
+            // Make an array that is 4 sec long of 48000hz * 2 channels audio and split it so that one can push and one cat pop
+            let mut sound_array: Vec<i16> = Vec::new();
+            let addr: &str = "0.0.0.0:9999";
+            let socket = UdpSocket::bind(addr)?;
+
+                let mut target_ip = String::new();
+                println!("What is the ip of the host you want to connect to?");
+                stdin().read_line(&mut target_ip).expect("Failed to read line");
+                let tmp_ip = target_ip.trim();
+                target_ip = format!("{}:9999", tmp_ip);
+                let target_addr: SocketAddr = target_ip.parse().expect("Failed to parse target address");
+                println!("Ready to send to: {}", target_ip);
+
+            move |data: &[i16], _: &cpal::InputCallbackInfo| {
+                // Read stream input audio
+                for &value in data {
+                    sound_array.push(value);
+                }
+
+                // Check if there is more than 0.2ms of sound data to transfer
+                if sound_array.len() >= TARGET_SAMPLES {
+                    let samples = &sound_array[0..TARGET_SAMPLES];
+                    let mut buf = [0u8; TARGET_BYTES];
+                    for (index, sample) in samples.iter().enumerate() {
+                        let byte = sample.to_le_bytes();
+                        buf[index * 2] = byte[0];
+                        buf[index * 2 + 1] = byte[1];
+                    }
+
+                    socket.send_to(&buf, target_addr).expect("Could not send udp");
+                }
+            }
+        }, 
+        move|err|{
+            // React to errors
+            eprintln!("Input stream error: {}", err);
+        }, 
+        None 
+        // Timeout for stream initialization: None = wait indefinitively. Some(Duration) = time to wait for the backend
+    ).expect("Failed to unwrap the input stream");
+
+    input_stream.play().unwrap();
+
     Ok(())
 }
 
